e409bb57bae59b0bd22f5dea4ffa8f67c779aff8
adamRevans <adame84@nycstudents.net>
2025-12-16 11:48:49 -0500

    Update Text_Editor.py

--- Text_Editor.py
+++ Text_Editor.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkmacosx import Button
+from tkinter import scrolledtext
 #Existance
 
 # Create Window
@@ -15,15 +16,15 @@ download_button = Button(root, text='Download Current Text File')
 files_button = Button(root, text='Select Text File')
 
 #Text Box
-writing_here = Text(root)
+text_area = scrolledtext.ScrolledText(root, width = 70, height = 20,) #Testing
 
 #Placing into Window
+text_area.grid(row=2,columnspan=3) #Testing
 save_button.grid(row=0, column=0)
 download_button.grid(row=0,column=1)
 files_button.grid(row=0,column=2)
 
-writing_here.grid(row=1,columnspan=3)
-
 
 #Make root work
+text_area.focus()#Testing
 root.mainloop()
